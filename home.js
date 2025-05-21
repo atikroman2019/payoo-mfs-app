7a887d8b99459d9aeb0bada44062f0160dcdb859
atikroman2019 <atikroman2016@gmail.comgmail.com>
2025-05-21 23:56:47 +0600

    add-money to the current-balance

---
+++ home.js
@@ -0,0 +1,11 @@
+// step-1: add eventlistener to the add funds button
+
+const addFundsButton = document.getElementById('add-funds-button').addEventListener('click', function(event){
+    event.preventDefault();
+    // step-2: get the data from amount & pin input field
+    const inputAmount = document.getElementById('input-amount').value
+    const inputPin = document.getElementById('input-pin').value
+
+    
+
+})
